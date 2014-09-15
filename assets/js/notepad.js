78915757224e8edc78a26dbdcaa29398f4668f5f
Jakub Malczak <j.malczak@samsung.local>
2014-09-15 14:19:53 +0200

    Ported some posts from old blog

--- assets/js/notepad.js
+++ assets/js/notepad.js
@@ -10,61 +10,61 @@ var notepad = (function ($) {
         postBgImages = '.bg-img img',
         postCoverImg = '.notepad-post-header .bg-img',
 
-    // post animations on homepage
-    indexPostAnimate = function () {
-        if ($(indexPostClass).length) {
-            $(indexPostClass).each(function () {
-            var postPos = $(this).offset().top;
-            var topOfWindow = $(window).scrollTop(),
-                windowHeight = $(window).height();
-                if (postPos < topOfWindow + (windowHeight/ 1.4)) {
-                    $(this).addClass('fadeInDown');
-                }
-            });
-        }
-    },
+        // post animations on homepage
+        indexPostAnimate = function () {
+            if ($(indexPostClass).length) {
+                $(indexPostClass).each(function () {
+                    var postPos = $(this).offset().top;
+                    var topOfWindow = $(window).scrollTop(),
+                        windowHeight = $(window).height();
+                    if (postPos < topOfWindow + (windowHeight / 1.4)) {
+                        $(this).addClass('fadeInDown');
+                    }
+                });
+            }
+        },
 
-    mobileMenu = function () {
-        if($(mainMenu).length) {
-            $(mobileMenuButton).on('click', function(e){
-                e.preventDefault();
-                $(mainMenu).addClass('opened');
-            });
-            $(mobileMenuCloseButton).on('click', function(e){
-                e.preventDefault();
-                $(mainMenu).removeClass('opened');
-            });
-        }
-    },
+        mobileMenu = function () {
+            if ($(mainMenu).length) {
+                $(mobileMenuButton).on('click', function (e) {
+                    e.preventDefault();
+                    $(mainMenu).addClass('opened');
+                });
+                $(mobileMenuCloseButton).on('click', function (e) {
+                    e.preventDefault();
+                    $(mainMenu).removeClass('opened');
+                });
+            }
+        },
 
-    headerTitlesBackgroundCheck = function () {
-        if ($(bgCheckClass).length && $(postBgImages).length) {
-            BackgroundCheck.init({
-                targets: bgCheckClass,
-                images: postBgImages
-            });
-        }
-    },
+        headerTitlesBackgroundCheck = function () {
+            if ($(bgCheckClass).length && $(postBgImages).length) {
+                BackgroundCheck.init({
+                    targets: bgCheckClass,
+                    images: postBgImages
+                });
+            }
+        },
 
-    postHeaderCoverImg = function () {
-        var coverImage = $('[alt=cover-image]');
-        if (coverImage.length) {
-            $(postCoverImg).append('<img src="' + coverImage.attr('src') + '">');
-            coverImage.remove();
-        }
-    },
+        postHeaderCoverImg = function () {
+            var coverImage = $('[alt=cover-image]');
+            if (coverImage.length) {
+                $(postCoverImg).append('<img src="' + coverImage.attr('src') + '">');
+                coverImage.remove();
+            }
+        },
 
-    // notepad javascripts initialization
-    init = function () {
-        indexPostAnimate();
-        $(window).on('scroll', function() {
+        // notepad javascripts initialization
+        init = function () {
             indexPostAnimate();
-        });
-        postHeaderCoverImg();
-        mobileMenu();
-        headerTitlesBackgroundCheck();
-        $('p:has(> img)').addClass('with-image');
-    };
+            $(window).on('scroll', function () {
+                indexPostAnimate();
+            });
+            postHeaderCoverImg();
+            mobileMenu();
+            headerTitlesBackgroundCheck();
+            $('p:has(> img)').addClass('with-image');
+        };
 
     return {
         init: init
