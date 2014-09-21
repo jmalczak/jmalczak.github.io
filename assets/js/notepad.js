46778b5cc807597a5c24bd29ad72083148d1b8ad
Jakub Malczak <jmalczak@gmail.com>
2014-09-21 16:46:48 +0200

    style change

--- assets/js/notepad.js
+++ assets/js/notepad.js
@@ -2,27 +2,12 @@
 
 var notepad = (function ($) {
 
-    var indexPostClass = '.notepad-index-post',
-        mobileMenuButton = '.notepad-mobile-menu a',
+    var mobileMenuButton = '.notepad-mobile-menu a',
         mobileMenuCloseButton = '.notepad-mobile-close-btn',
         mainMenu = '.notepad-menu',
         bgCheckClass = '.bg-check',
         postBgImages = '.bg-img img',
         postCoverImg = '.notepad-post-header .bg-img',
-
-        // post animations on homepage
-        indexPostAnimate = function () {
-            if ($(indexPostClass).length) {
-                $(indexPostClass).each(function () {
-                    var postPos = $(this).offset().top;
-                    var topOfWindow = $(window).scrollTop(),
-                        windowHeight = $(window).height();
-                    if (postPos < topOfWindow + (windowHeight / 1.4)) {
-                        $(this).addClass('fadeInDown');
-                    }
-                });
-            }
-        },
 
         mobileMenu = function () {
             if ($(mainMenu).length) {
@@ -56,10 +41,6 @@ var notepad = (function ($) {
 
         // notepad javascripts initialization
         init = function () {
-            indexPostAnimate();
-            $(window).on('scroll', function () {
-                indexPostAnimate();
-            });
             postHeaderCoverImg();
             mobileMenu();
             headerTitlesBackgroundCheck();
